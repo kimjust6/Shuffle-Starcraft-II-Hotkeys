b5bd12a272779d1de37480322357798e92c37342
MLT-Acer <kimjust6@gmail.com>
2019-01-18 17:32:47 -0500

    fixed user input

--- Shuffle.py
+++ Shuffle.py
@@ -1,36 +1,39 @@
 
 def main():
-    #importing libraries needed for makeShuffle function
-    import time
-    import sys
-    import random
+
 
     #importing libraries needed for main
     import shutil
     import os
 
     #prompt user for directory that we will be using
-    base_file_directory = input("Please enter the file directory for your starcraft folder:\nLeaving this empty will assume it is in the default location\n")
-    print(base_file_directory)
+    base_file_directory = raw_input("Please enter the file directory for your starcraft folder:\nLeaving this empty will assume it is in the default location.\n")
 
     #check for valid user input
     #if it is empty, assume the default directory
-    if base_file_directory == "\n":
-        print("boy that is a new line")
+    if base_file_directory == "":
+        print("boy that is an empty string!\nAssuming default location:")
+        #base_file_directory = "D:\Documents\Starcraft II\accounts"
         base_file_directory = "Z:\\My Drive\\Dropbox\\New folder (3)\\Starcraft II\\Accounts\\"
-    elif base_file_directory == "":
-        print("boy that is an empty string")
+        print(base_file_directory)
+
+
 
     #the accountIds of the accounts you want to copy and shuffle hotkeys to
     #this should be changed so that it copies them automatically
     accounts = ["1736517", "70056340", "72705267", "77732427", "85434961", "86373425", "86492430", "87808971", "89446409", "89662727", "126384011", "315855594", "350403565", "351690861", "377437837", "377785739", "377793407", "379201447", "379209204", "379220901", "379220943", "379252463", "379252496", "379266120", "379906113", "381274912", "381827850", "381857282", "381867033", "448356624"]
     for x in accounts:
-        makeShuffle()
-        shutil.move( "C:\\Users\\MLT-Acer\\Downloads\\MLT-Shuffle.SC2Hotkeys", "Z:\\My Drive\\Dropbox\\New folder (3)\\Starcraft II\\Accounts\\" + x + "\Hotkeys\MLT.SC2Hotkeys")
+        #check if account is the source account of the shuffle (56959549) and skip if that is the case
+        if x != 56959549:
+            makeShuffle()
+            shutil.move( "C:\\Users\\MLT-Acer\\Downloads\\MLT-Shuffle.SC2Hotkeys", base_file_directory + x + "\Hotkeys\MLT.SC2Hotkeys")
 
 
 def makeShuffle():
-
+    #importing libraries needed for makeShuffle function
+    import time
+    import sys
+    import random
     #path to original file
     inputfile = open("Z:\\My Drive\\Dropbox\\New folder (3)\\Starcraft II\\accounts\\56959549\\Hotkeys\\MLT.SC2Hotkeys","r")
 
